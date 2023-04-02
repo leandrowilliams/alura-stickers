1396a375350d873dcf6fc1ad2be12a88dddca310
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-04-02 15:42:58 -0300

    update da aula 03, para poder fazer o restante das aulas

--- src/App.java
+++ src/App.java
@@ -14,33 +14,42 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
         //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+//        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 
-        // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        API api = API.IMDB_TOP_SERIES;
+
+        String url = api.getUrl();
+        ExtratorDeConteudo extrator = api.getExtrator();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+//        URI endereco = URI.create(url);
+//        var client = HttpClient.newHttpClient();
+//        var request = HttpRequest.newBuilder(endereco).GET().build();
+//        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+//        String body = response.body();
+
+//        // extrair só os dados que interessam (titulo, poster, classificação)
+//        var parser = new JsonParser();
+//        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
         // exibir e manipular os dados
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradoraDeFigurinhas();
 
-        for (int i = 0; i < 10; i++) {
-            Map<String, String> filme = listaDeFilmes.get(i);
+        for (int i = 0; i < 3; i++) {
 
-                String urlImagem = filme.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
-                String titulo = filme.get("title");
+            Conteudo conteudo = conteudos.get(i);
 
-                InputStream inputStream = new URL(urlImagem).openStream();
-                String nomeArquivo = "saida/" + titulo + ".png";
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.titulo() + ".png";
 
-                geradora.cria(inputStream, nomeArquivo);
+            geradora.cria(inputStream, nomeArquivo);
 
-                System.out.println(titulo);
-                System.out.println();
+            System.out.println(conteudo.titulo());
+            System.out.println();
 
         }
     }
---
+++ src/Conteudo.java
@@ -0,0 +1,22 @@
+public record Conteudo(String titulo, String urlImagem) {
+
+}
+
+
+//public class Conteudo {
+//    private final String titulo;
+//    private final String urlImagem;
+//
+//    public Conteudo(String titulo, String urlImagem) {
+//        this.titulo = titulo;
+//        this.urlImagem = urlImagem;
+//    }
+//
+//    public String getTitulo() {
+//        return titulo;
+//    }
+//
+//    public String getUrlImagem() {
+//        return urlImagem;
+//    }
+//}
